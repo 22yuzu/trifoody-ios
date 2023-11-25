421133602f89d7bb1e77ea51f3c65f208010f949
22yuzu <yuzubook2@icloud.com>
2023-11-25 16:04:30 +0900

    Initial Commit

---
+++ trifoody_ios/trifoody_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  trifoody_iosApp.swift
+//  trifoody_ios
+//
+//  Created by 冨岡哲平 on 2023/11/25.
+//
+
+import SwiftUI
+
+@main
+struct trifoody_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
